91abbe14edcc51f928318a2d4f53e6f52137b996
Mark <markberry867@gmail.com>
2018-01-31 00:36:13 -0600

    Style updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { NgxGraphModule } from '@swimlane/ngx-graph'
 import { TreeModule } from 'ng2-tree'
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar'
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar'
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar'
 
 import { AppComponent } from './app.component'
 import { DashboardComponent } from './components/dashboard/dashboard.component'
@@ -21,6 +24,10 @@ import { DagExplorerComponent } from './components/dag-explorer/dag-explorer.com
 import { DagExplorerFilesListComponent } from './components/dag-explorer-files-list/dag-explorer-files-list.component'
 
 import { IpfsService } from './services/ipfs.service'
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+
+}
 
 const appRoutes: Routes = [
   {
@@ -68,9 +75,14 @@ const appRoutes: Routes = [
     ),
     FlexLayoutModule,
     NgxGraphModule,
-    TreeModule
+    TreeModule,
+    PerfectScrollbarModule
   ],
   providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    },
     IpfsService
   ],
   bootstrap: [AppComponent]
