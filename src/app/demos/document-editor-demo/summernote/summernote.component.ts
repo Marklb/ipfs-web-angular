73da9a9e1a8a57ac18a6ea4201b458bac97ab286
Mark <markberry867@gmail.com>
2018-03-07 00:34:39 -0600

    Add some summernote plugins

--- src/app/demos/document-editor-demo/summernote/summernote.component.ts
+++ src/app/demos/document-editor-demo/summernote/summernote.component.ts
@@ -24,16 +24,62 @@ export class SummernoteComponent implements OnInit {
       $(document).ready(function() {
         $('#summernote').summernote({
           toolbar: [
+            ['paperSize', ['paperSize']],
             ['style', ['style']],
             ['font', ['bold', 'underline', 'clear']],
             ['fontname', ['fontname']],
             ['color', ['color']],
-            ['para', ['ul', 'ol', 'paragraph']],
+            ['para', ['ul', 'ol', 'listStyles', 'paragraph']],
             ['table', ['table']],
-            ['insert', ['link', 'picture', 'video']],
+            ['insert', ['link', 'picture', 'video', 'nugget', 'template']],
+            ['pagebreak', ['pagebreak']],
+            // ['custom', ['findnreplace']],
             ['misc', ['print']],
             ['view', ['fullscreen', 'codeview', 'help']]
-          ]
+          ],
+          popatmouse: false,
+          popover: {
+            image: [
+              ['imagesize', ['imageSize100', 'imageSize50', 'imageSize25']],
+              ['float', ['floatLeft', 'floatRight', 'floatNone']],
+              ['remove', ['removeMedia']]
+            ],
+            link: [
+              ['link', ['linkDialogShow', 'unlink']]
+            ],
+            table: [
+              ['add', ['addRowDown', 'addRowUp', 'addColLeft', 'addColRight']],
+              ['delete', ['deleteRow', 'deleteCol', 'deleteTable']],
+              ['custom', ['tableHeader']]
+            ],
+            air: [
+              ['color', ['color']],
+              ['font', ['bold', 'underline', 'clear']],
+              ['para', ['ul', 'paragraph']],
+              ['table', ['table']],
+              ['insert', ['link', 'picture']]
+            ]
+          },
+          nugget: {
+            list: [ // list of your nuggets
+              '[[code nugget 1]]',
+              '[[code nugget 2]]',
+              '[[code nugget 2]]'
+            ]
+          },
+          template: {
+            path: '/assets/summernote/tpls', // path to your template folder
+
+            /*
+             * list of your templates
+             * key is the html file name (without .html extension)
+             * value is the label shown in the editor
+             */
+            list: {
+                'label-success': 'Success label', // path is : /summernote/tpls/label-success.html
+                'label-error': 'Error label'
+            }
+          }
         })
       })
     })
@@ -51,14 +97,27 @@ export class SummernoteComponent implements OnInit {
     await this.loadExternalFilesService.loadFile('http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js')
     await this.loadExternalFilesService.loadFile('http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-lite.css')
     await this.loadExternalFilesService.loadFile('http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-lite.js')
-    await this.loadExternalFilesService.loadFile('/assets/summernote-ext-print.js')
+    await this.loadExternalFilesService.loadFile('/assets/summernote/summernote-ext-print.js')
+    await this.loadExternalFilesService.loadFile('/assets/summernote/summernote-ext-nugget.js')
+    await this.loadExternalFilesService.loadFile('/assets/summernote/summernote-ext-template.js')
+    await this.loadExternalFilesService.loadFile('/assets/summernote/summernote-list-styles.js')
+    await this.loadExternalFilesService.loadFile('/assets/summernote/summernote-list-styles.css')
+    await this.loadExternalFilesService.loadFile('/assets/summernote/summernote-pagebreak.js')
+    await this.loadExternalFilesService.loadFile('/assets/summernote/summernote-paper-size.js')
+    await this.loadExternalFilesService.loadFile('/assets/summernote/summernote-table-headers.js')
+    // await this.loadExternalFilesService.loadFile('/assets/summernote/summernote-text-findnreplace.js')
+    // await this.loadExternalFilesService.loadFile('/assets/summernote/.js')
+    // await this.loadExternalFilesService.loadFile('/assets/summernote/.js')
+    // await this.loadExternalFilesService.loadFile('/assets/summernote/.js')
+    // await this.loadExternalFilesService.loadFile('/assets/summernote/.js')
+
     this.loading = false
   }
 
   public save() {
     const element = document.querySelector('.note-editable')
     html2pdf(element, {
-      margin:       1,
+      margin:       0,
       filename:     'myfile.pdf',
       image:        { type: 'jpeg', quality: 0.98 },
       html2canvas:  { dpi: 192, letterRendering: true },
